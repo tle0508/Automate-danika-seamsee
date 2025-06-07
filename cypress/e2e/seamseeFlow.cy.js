d2564c316c44cc5770b624840671201c0a055e37
sirun <tlezazazazaza@gmail.com>
2025-06-07 10:42:58 +0700

    add 1 case

---
+++ cypress/e2e/seamseeFlow.cy.js
@@ -0,0 +1,12 @@
+describe('seamsee Flow Test', () => {
+  it('Show Qr Code after confirmm', () => { 
+    cy.visit('https://dev-only-danika-seamsee.0jpzrz.easypanel.host/')
+    cy.contains('button', 'เข้าสู่หน้าเซียมซี').click()
+    cy.contains('span', 'ไทย').click()
+    cy.contains('span', 'วัดพระธาตุดอยคำ').click()
+    cy.contains('span', '19 บาท').click()
+    cy.contains('button', 'ยืนยัน').click()
+    cy.wait(3000)
+    cy.get('img.rounded-\\[10\\%\\]').should('be.visible')
+  })
+})
